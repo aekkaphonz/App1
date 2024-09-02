645f89817b557d3cff086edb6acfe1a4859897c0
aekkaphonz <boxxing34@gmail.com>
2024-09-02 11:25:13 +0700

    content , header2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
-import MessageBox from './state-func'
+import { userContext } from './context'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
+
 
 
 export default function App(){
-  return<MessageBox/>
-
+  let[user,setUser] = React.useState('')
+  return(
+  <userContext.Provider value={[user,setUser]}>
+    <Header2/>
+    <Content2/>
+  </userContext.Provider>
+  )
 }
